724a60482356c102d90e45936fe2a9b1df7300dd
yin <gil.mifsud@gmail.com>
2017-10-27 14:56:52 +0200

    Cleaned vibes2 css and added filigrane to photos

--- Web/Vibes2/scripts/user.js
+++ Web/Vibes2/scripts/user.js
@@ -1,71 +1,95 @@
-//BrowserSize
-var browsersize=800;
+$(document).ready(function() {
 
-//Onload hide menu
-menuformat();
 
-//Handle window resize on menu items
-$(window).on("resize", function(){
+	//BrowserSize
+	var browsersize=800;
+
+	//Onload hide menu
 	menuformat();
-});
 
-//funstion to handle menus
-function menuformat(){
-	if($(document).width() >= browsersize)
-	{
-		$("nav ul").show();
-		$("#hamburger").hide();
-		$("nav ul").addClass("normalmenu");
-		$("nav ul").removeClass("burgermenu");
-	}
-	else
-	{
-		$("nav ul").hide();
-		$("#hamburger").show();
-		$("nav ul").removeClass("normalmenu");
-		$("nav ul").addClass("burgermenu");
-	}
-}
-
-// Burger Click
-$('#hamburger').on("click", function(){
-	$("nav ul").slideDown(400);
-	$('#hamburger').hide();
-});
-
-//Hide rolldown menu on mouse off
-$("header").mouseleave(function(){
-
-	if(document.body.clientWidth <= browsersize)
-	{
-		$("nav ul").slideUp(400, null, function(){
-									$('#hamburger').fadeIn(100);
-								});
-	}
-
-});
-
-
-////////////////////////////////
-// Table
-
-//resize
-/*
-$(window).resize(function(){
-	$(".photoTableDiv").css("height",  $(".photoTableTd").css("width") );
-
-});
-*/
-//
-
-
-
-
-////////////////////////////////
-//Jquery
-$( function() {
-	$( "#accordion" ).accordion({
-	  heightStyle: "content"
+	//Handle window resize on menu items
+	$(window).on("resize", function(){
+		menuformat();
 	});
- } );
 
+	//funstion to handle menus
+	function menuformat(){
+		if($(document).width() >= browsersize)
+		{
+			$("nav ul").show();
+			$("#hamburger").hide();
+			$("nav ul").addClass("normalmenu");
+			$("nav ul").removeClass("burgermenu");
+		}
+		else
+		{
+			$("nav ul").hide();
+			$("#hamburger").show();
+			$("nav ul").removeClass("normalmenu");
+			$("nav ul").addClass("burgermenu");
+		}
+	}
+
+	// Burger Click
+	$('#hamburger').on("click", function(){
+		$("nav ul").slideDown(400);
+		$('#hamburger').hide();
+	});
+
+	//Hide rolldown menu on mouse off
+	$("header").mouseleave(function(){
+
+		if(document.body.clientWidth <= browsersize)
+		{
+			$("nav ul").slideUp(400, null, function(){
+										$('#hamburger').fadeIn(100);
+									});
+		}
+
+	});
+
+
+
+	////////////////////////////////
+	// Photo carrosel
+
+	function playButton(imageID){
+		console.log("calledf");
+		$('#img'+imageID).mouseenter(function() {
+			console.log("mouse over", $(this))
+			$('#fil'+imageID).fadeIn(200);
+		}).mouseleave(function(){
+			$('#fil'+imageID).fadeOut(200);
+		} );
+	};
+
+	function initializeFiligranes(){
+		for (i = 1; i < 9; i++)
+		{
+			playButton(i);
+		}
+	}
+
+	initializeFiligranes();
+
+	//resize
+	/*
+	$(window).resize(function(){
+		$(".photoTableDiv").css("height",  $(".photoTableTd").css("width") );
+
+	});
+	*/
+	//
+
+
+
+
+	////////////////////////////////
+	//Jquery
+	$( function() {
+		$( "#accordion" ).accordion({
+		  heightStyle: "content"
+		});
+	 } );
+
+});
